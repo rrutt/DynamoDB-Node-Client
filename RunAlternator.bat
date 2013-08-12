21f90710607442d2a59c0cea7fbf960150a2f21c
Rick Rutt <RuttR@Valassis.com>
2013-08-12 09:22:08 -0400

    Changed RunAlternator scripts to run 0.6.1-SNAPSHOT version.

--- RunAlternator.bat
+++ RunAlternator.bat
@@ -2,7 +2,7 @@
 
 cls
 
-set ALTERNATOR_VERSION=0.5.2-SNAPSHOT
+set ALTERNATOR_VERSION=0.6.1-SNAPSHOT
 set ALTERNATOR_HOME=%USERPROFILE%\.m2\repository\com\michelboudreau\alternator\%ALTERNATOR_VERSION%
 
 set JAVA_OPTS=-Xms256m -Xmx2048m
